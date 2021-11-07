065bebdef5d18b6ac610eb4c5230e048d40f0688
Sean Sundberg <seansund@us.ibm.com>
2021-11-07 16:00:46 -0600

    Updates output variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
 output "ingress_host" {
   description = "The ingress host for the Artifactory instance"
   value       = local.ingress_host
-  depends_on  = [helm_release.artifactory]
+  depends_on  = [null_resource.wait-for-config-job]
 }
 
 output "ingress_url" {
   description = "The ingress url for the Artifactory instance"
   value       = local.ingress_url
-  depends_on  = [helm_release.artifactory]
+  depends_on  = [null_resource.wait-for-config-job]
 }
 
 output "config_name" {
   description = "The name of the secret created to store the url"
   value       = local.config_name
-  depends_on  = [helm_release.artifactory]
+  depends_on  = [null_resource.wait-for-config-job]
 }
 
 output "secret_name" {
   description = "The name of the secret created to store the credentials"
   value       = local.secret_name
-  depends_on  = [helm_release.artifactory]
+  depends_on  = [null_resource.wait-for-config-job]
 }
