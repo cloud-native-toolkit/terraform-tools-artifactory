0824348e479ba3d0b90a79ab3f66ebea0e2e1962
Sean Sundberg <seansund@us.ibm.com>
2021-11-07 19:47:38 -0600

    Fixes test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-console-link-job.tf
+++ test/stages/stage1-console-link-job.tf
@@ -1,6 +1,6 @@
 module "console-link-job" {
   source = "github.com/cloud-native-toolkit/terraform-k8s-console-link-job.git"
 
-  namespace = module.dev_tools_namespace.namespace
+  namespace = module.dev_tools_namespace.name
   cluster_config_file = module.dev_cluster.config_file_path
 }
